061a51f8f31e0a50271399500a53c97d10737454
rudo mapfumba <rudomaru25@gmail.com>
2020-12-12 00:11:31 +0800

    add

--- src/components/AddTestimony.js
+++ src/components/AddTestimony.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react' 
+import { useForm } from "react-hook-form";
+
 
 function AddTestimony() {
+    const [name, setname] = useState("");
+    const [message, setmessage] = useState();
+    const { register, handleSubmit,errors } = useForm();
+
+    const handleSubmitForm = () =>{
+        console.log("submitted")
+        alert("testimony successfully submited")
+        setmessage("")
+        setname("")
+    }
+
     return (
         <div className="section addTestimony">
             <div className="row">
@@ -9,10 +22,15 @@ function AddTestimony() {
                     <h1>Testify</h1>
                     <hr className="centered"/>
                 </div>
-                <form action="" className="col-sm-12 col-md-6">
-                    <input className="form-input" type="text" placeholder="Your Name"/>
-                    <textarea className="form-input"  placeholder="Type here..." rows="10"></textarea>
-                    <button className="button info-button">Submit</button>
+                <form  onSubmit={handleSubmit(handleSubmitForm )}   className="col-sm-12 col-md-6">
+                    <input ref={register({ required: true })}  name="name" value={name} onChange={e => setname(e.target.value)} className="form-input" type="text" placeholder="Your Name"/>
+                    {errors.name && <span className="text-warning"> This field  is required</span>}
+                    <textarea ref={register({ required: true })}  name="message" value={message} onChange={e => setmessage(e.target.value)} className="form-input"  placeholder="Type here..." rows="10"></textarea>
+                    {errors.message && <span className="text-warning"> This field  is required</span>}
+                    <div>
+                    <button type="submit" className="button info-button">Submit</button>
+                    </div>
+                    
                 </form>
             </div>
         </div>
