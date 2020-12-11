17df8cc87e6c5fddc81a0e2f78682dd874a3f695
rudo mapfumba <rudomaru25@gmail.com>
2020-12-11 17:15:50 +0800

    add

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import AboutUs from '../components/About';
 import AdminLetter from '../components/AdminLetter';
-import Staff from '../components/Staff';
+//import Staff from '../components/Staff';
 import PageHeader from '../components/PageHeader';
 
 function About() {
